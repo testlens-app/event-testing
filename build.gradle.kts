fbc3e15df9a7bff282d6ad66f6fdf69a2145d3c5
Benedikt Ritter <beneritter@gmail.com>
2025-03-27 10:12:35 +0100

    Log more error context to stdout
    We're running out of GitHub actions storage, so we can't upload test
    results at the moment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 plugins { id("java-library") }
 
 testing.suites.named<JvmTestSuite>("test") {
     useJUnitJupiter()
+    targets {
+        testTasks.configureEach {
+            testLogging {
+                exceptionFormat = TestExceptionFormat.FULL
+            }
+        }
+    }
 }
