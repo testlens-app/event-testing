d0af82591b492e68087781b9300296c40d961804
Marc Philipp <mail@marcphilipp.de>
2025-11-11 11:52:30 +0100

    Make tests succeed on retries

--- src/test/java/org/example/SampleTests.java
+++ src/test/java/org/example/SampleTests.java
@@ -2,12 +2,34 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestReporter;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 abstract class SampleTests {
 
     private static boolean firstRunFinished = false;
+
+    private static Path markerFile;
+    private static boolean markerFileDoesNotExist;
+
+    @BeforeAll
+    static void checkForMarkerFile(TestInfo testInfo) {
+        markerFile = Path.of("build/marker-%s.txt".formatted(testInfo.getTestClass().orElseThrow().getSimpleName()));
+        markerFileDoesNotExist = Files.notExists(markerFile);
+    }
+
+    @AfterAll
+    static void createMarkerFile() throws Exception {
+        if (markerFileDoesNotExist) {
+            Files.createFile(markerFile);
+        }
+    }
 
     @Test
     void test1() {
@@ -27,7 +49,7 @@ abstract class SampleTests {
         }
 
         firstRunFinished = true;
-        fail();
+        maybeFail();
     }
 
     @Test
@@ -39,6 +61,12 @@ abstract class SampleTests {
     @Test
     void test5_failing() {
         System.out.println("it fails");
-        fail();
+        maybeFail();
+    }
+
+    private static void maybeFail() {
+        if (markerFileDoesNotExist) {
+            fail();
+        }
     }
 }
