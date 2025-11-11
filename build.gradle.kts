0da3335c5c308c2b15a6797c4af8af31e773ff24
Marc Philipp <mail@marcphilipp.de>
2025-11-11 09:55:31 +0100

    Configure test-retry plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
-plugins { id("java-library") }
+plugins {
+    id("java-library")
+    id("org.gradle.test-retry") version "1.6.4"
+}
 
 testing.suites.named<JvmTestSuite>("test") {
     useJUnitJupiter()
@@ -11,6 +14,11 @@ testing.suites.named<JvmTestSuite>("test") {
                 minGranularity = 0
                 events = setOf(STANDARD_OUT, STANDARD_ERROR, FAILED)
                 exceptionFormat = TestExceptionFormat.FULL
+            }
+            retry {
+                maxRetries = providers.gradleProperty("maxRetries")
+                    .map { it.toInt() }
+                    .orElse(0)
             }
         }
     }
